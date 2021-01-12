76c5ef6401f5bcf8da3c6d7c25ddcce46ba08d4d
Yazdan Musa <yazdanmusa@gmail.com>
2021-01-12 20:21:50 +0100

    temp

---
+++ public/app.js
@@ -0,0 +1,5 @@
+function toggleCart() {
+    miniCart = document.getElementById("dropdown");
+    miniCart.style.display == "flex" miniCart.style.display="none";
+    miniCart.style.display = "flex";
+}
--- router.ts
+++ router.ts
@@ -1,18 +1,6 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import articleController from "./controllers/ArticleController.ts";
-import cartController from "./controllers/CartController.ts";
-import siteController from "./controllers/SiteController.ts";
 
 const router = new Router();
 
-router.get("/", siteController.Index);
-router.get("/ArticleDetail/:id", siteController.ArticleDetail);
-router.get("/Cart", siteController.Cart);
-
-// Router Def for Articles
-router.get("/api/getallarticles", articleController.getAllArticles);
-router.get("/api/article/:id", articleController.article);
-router.post("/api/createarticle", articleController.createArticle);
-router.delete("/api/deletearticle/:id", articleController.deleteArticle);
 
 export default router;
