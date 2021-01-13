b2ac1b16915760bb6dd411bb3d3cd4208b99a93d
Zayden16 <yazdanmusa@gmail.com>
2021-01-13 17:22:30 +0100

    fixed display of no cart

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,9 @@
-class CartItem {
-    constructor(artId, amount){
-        this.ArticleId = artId;
-        this.Amount = amount;
-    }
-}
-console.log("readsy");
 let toggler = document.getElementById("cart-toggler");
 let minicart = document.getElementById("minicart");
 toggler.addEventListener("click", function() {
-    if (minicart.style.display === "none") {
+    if (minicart.style.display == "none" || minicart.style.display == "") {
         minicart.style.display = "flex";
-        console.log("changed disp to flex");
     } else {
         minicart.style.display = "none";
-        console.log("changed disp to none");
     }
 });
--- public/js/app.ts
+++ public/js/app.ts
@@ -1,29 +1,24 @@
 import { CartItem } from "../../models/CartItem.ts"
 
-console.log("readsy");
 let toggler = document.getElementById("cart-toggler");
 let minicart = document.getElementById("minicart");
 
 toggler.addEventListener("click", function() {
-    if(minicart.style.display === "none"){
+    if (minicart.style.display == "none" || minicart.style.display == "") {
         minicart.style.display = "flex";
-        console.log("changed disp to flex");
-        
     }
     else {
         minicart.style.display = "none";
-        console.log("changed disp to none");
-        
     }
 });
 
 
-export async function addToCart(artId: string) {
-    const amount = document.getElementById(`article-counter-${artId}`);
-    const cartItem = new CartItem(artId, amount);
-    await fetch("/api/cart/addItem", {
-        method: 'POST',
-        mode: 'no-cors',
-        body: JSON.stringify(cartItem)
-    });
-}
+// export async function addToCart(artId: string) {
+//     const amount = document.getElementById(`article-counter-${artId}`);
+//     const cartItem = new CartItem(artId, amount);
+//     await fetch("/api/cart/addItem", {
+//         method: 'POST',
+//         mode: 'no-cors',
+//         body: JSON.stringify(cartItem)
+//     });
+// }
